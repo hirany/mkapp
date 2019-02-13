bc71766dbc5586b451585bddfcfbd5f6f2ebf57e
hirany <s17t260@stu.kagawa-u.ac.jp>
2019-02-13 19:14:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# mkapp
+
+cliプログラムのテンプレートを作成する
+
+使う前
+```
+git clone https://github.com/hirany/mkapp
+cd mkapp
+go install
+```
+
+使う時
+```
+mkapp appname
+```
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+const (
+	perm = 755
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "lt"
+	app.Usage = "show directory"
+	app.Action = func(c *cli.Context) {
+		err := mkapp(c.Args()[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	app.Run(os.Args)
+}
+
+func mkapp(cliName string) error {
+	err := os.Mkdir(cliName, perm)
+	if err != nil {
+		return err
+	}
+	err = os.Chdir(cliName)
+	if err != nil {
+		return err
+	}
+	file, err := os.Create("main.go")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	inCode := `package main
+
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "app name"
+	app.Usage = "how to use"
+	app.Action = func(c *cli.Context) {
+		run(c.Args())
+	}
+	app.Run(os.Args)
+}
+
+func run(args cli.Args) {
+}`
+	_, err = file.WriteString(inCode)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
